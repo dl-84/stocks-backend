dee9ab8227a7c45297f48ddc5548ce082b823502
lindnerd <d.lindner@reinhausen.com>
2023-10-29 22:45:12 +0100

    docker an doku

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -5,6 +5,8 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient {
     constructor(private configService: ConfigService) {
+        console.log(configService.get('DATABASE_URL'));
+
         super({
             datasources: {
                 db: {
